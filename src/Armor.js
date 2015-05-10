deedfe107a96d56b48f0b5b1209b1167774c080c
Mikolay <mikollaay@Gmail.com>
2015-05-10 18:53:34 +0300

    add functional

--- src/Armor.js
+++ src/Armor.js
@@ -5,90 +5,135 @@
 
 (function() {
 
-"use strict";
+  "use strict";
 
-var _istag = /^<(\w+)\s*\/?>(?:<\/\1>|)$/;
+  var me = this;
 
-var A = function(selector , context /*optional*/) {
-  var r, p, c = context || document;
-  try {
-    if (A.isObject(selector)) {
-      r = selector;
-    } else if ((p = _istag.exec(selector))) {
-      r = document.createElement(p[1]);
-      if (A.isObject((c = context))) {
-        for (p in c) {
-          r.setAttribute(p, c[p]);
+  var _istag = /^<(\w+)\s*\/?>(?:<\/\1>|)$/;
+  var prevarmorjs = me.armorjs;
+  var prevA = me.A;
+
+  /**
+   * [A factory function]
+   * @selector {String}
+   * @context {[Object]}
+   */
+  var A = function(selector, context) {
+    var r, p, c = context || document;
+    try {
+      if (A.isObject(selector)) {
+        r = selector;
+      } else if ((p = _istag.exec(selector))) {
+        r = document.createElement(p[1]);
+        if (A.isObject((c = context))) {
+          for (p in c) {
+            r.setAttribute(p, c[p]);
+          }
         }
+      } else {
+        r = c.querySelectorAll(selector);
+        if (!r || !r.length) return null;
+        if (A.isArrayType(r) && r.length == 1 && selector.substr(0, 1) == "#") r = r[0];
+      }
+      A.ext(r, A.domlst);
+    } catch (e) {
+      r = null;
+    }
+    return r;
+  };
+
+  A.noConflict = function() {
+    if (me.A === armorjs) {
+      me.A = prevA;
+    }
+
+    if (me.armorjs === armorjs) {
+      me.armorjs = prevarmorjs;
+    }
+
+    return A;
+  };
+
+  A.VERSION = '0.0.1';
+
+  A.ui = {};
+
+  me.A = me.armorjs = A;
+  /**
+   * [uid create unique id value]
+   * @return {[Number]}
+   */
+  A.uid = function() {
+    if (!this._sequence) this._sequence = (new Date()).valueOf();
+    this._sequence++;
+    return this._sequence;
+  };
+  /**
+   * [forEach description]
+   * @o  {Array}
+   * @callback  {Function}
+   * @return {Array}
+   */
+  A.each = A.forEach = function(o, callback) {
+    var i = 0,
+      l = o.length;
+    if (A.isArrayType(o)) {
+      for (; i < l; i++) {
+        if (callback.call(o[i], i, o[i]) === false) break;
       }
     } else {
-      r = c.querySelectorAll(selector);
-      if (!r || !r.length) return null;
-      if (A.isArrayType(r) && r.length == 1 && selector.substr(0, 1) == "#") r = r[0];
+      return callback.call(o, i, o);
     }
-    A.ext(r, A.domlst);
-  } catch (e) {
-    r = null;
-  }
-  return r;
-};
-
-window.A = window.armorjs = A;
-
-A.ui = {};
-
-A.each = function(o, callback) {
-  var i = 0,
-    l = o.length;
-  if (A.isArrayType(o)) {
-    for (; i < l; i++) {
-      if (callback.call(o[i], i, o[i]) === false) break;
+    return o;
+  };
+  /**
+   * [clone object]
+   * @param  {Object}
+   * @return {Object}
+   */
+  A.clone = function(obj) {
+    //obj = obj || {};
+    var c, i = 0,
+      l = obj.length;
+    if (!obj || "object" != typeof obj) return obj;
+    if (obj instanceof Date) {
+      c = new Date();
+      c.setTime(obj.getTime());
+      return c;
     }
-  } else {
-    return callback.call(o, i, o);
-  }
-  return o;
-};
-
-A.clone = function(obj) {
-  //obj = obj || {};
-  var c, i = 0,
-    l = obj.length;
-  if (!obj || "object" != typeof obj) return obj;
-  if (obj instanceof Date) {
-    c = new Date();
-    c.setTime(obj.getTime());
-    return c;
-  }
-  if (obj instanceof Array) {
-    c = [];
-    for (; i < l; i++) {
-      c[i] = A.clone(obj[i]);
+    if (obj instanceof Array) {
+      c = [];
+      for (; i < l; i++) {
+        c[i] = A.clone(obj[i]);
+      }
+      return c;
     }
-    return c;
-  }
-  if (obj instanceof Object) {
-    c = {};
-    for (var a in obj) {
-      if (obj.hasOwnProperty(a)) c[a] = A.clone(obj[a]);
+    if (obj instanceof Object) {
+      c = {};
+      for (var a in obj) {
+        if (obj.hasOwnProperty(a)) c[a] = A.clone(obj[a]);
+      }
+      return c;
     }
-    return c;
-  }
-  return obj;
-};
-
-A.ext = function(dest) {
-  var ss = Array.prototype.slice.call(arguments, 1),
-    p, i, s;
-  for (i = 0; i < ss.length; i++) {
-    s = ss[i] || {};
-    for (p in s) {
-      if (s.hasOwnProperty(p)) {
-        dest[p] = A.clone(s[p]);
+    return obj;
+  };
+  /**
+   * [ext description]
+   * @param  {[type]}
+   * @return {[type]}
+   */
+  A.ext = function(dest) {
+    var ss = Array.prototype.slice.call(arguments, 1),
+      p, i, s;
+    for (i = 0; i < ss.length; i++) {
+      s = ss[i] || {};
+      for (p in s) {
+        if (s.hasOwnProperty(p)) {
+          dest[p] = A.clone(s[p]);
+        }
       }
     }
-  }
-  return dest;
-};
+    return dest;
+  };
 
-})();
+}.call(this));
